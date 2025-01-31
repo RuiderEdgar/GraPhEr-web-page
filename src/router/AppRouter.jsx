7326195992c767574f0be77e6e0f1002fb505e02
RuiderEdgar <edgar.riveraj@gmail.com>
2025-01-30 22:27:12 -0600

    estructura de archivos

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,13 @@
+import { Route, Routes } from "react-router-dom";
+
+
+export const AppRouter = () => {
+  return (
+    <>
+      <Routes>
+        {/* <Route path="/*" element={<GrapherRoutes />} /> */}
+        
+      </Routes>
+    </>
+  )
+}
