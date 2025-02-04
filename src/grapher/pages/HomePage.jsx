a5a38dafe1cf8e83000b8c4020871d9bde387936
LuisNavaFisBio <ruisugarciafisbio@ciencias.unam.mx>
2025-02-03 20:56:13 -0600

    Cambio en homepage

--- src/grapher/pages/HomePage.jsx
+++ src/grapher/pages/HomePage.jsx
@@ -2,11 +2,23 @@ import './HomePage.css'
 
 export const HomePage = () => {
 	return (
-		<>
+		<body class="back">
 			<div class="container">
 				<div class="column">
-					<h1>GraPhEr</h1>
+					<h1 class="name">GraPhEr</h1>
 					<p class="title">Graficadora para Ecuaciones <br></br> Diferencias Parciales </p>
+
+					<p class="description">
+					GraPhEr crea una gráfica interactiva de la solución de<br></br>
+					una Ecuación Diferencial Parcial a partir de aproximar<br></br>
+					numéricamente la solución analítica ingresada.<br></br>
+					<br></br>
+					Además, provee diversas herramientas para visualizar<br></br>
+					y explorar esta gráfica, así como explorar la solución<br></br>
+					numérica obtenida.	
+					</p>
+
+					<input type="button" value="Descargar GraPhEr" class="download"/>
 				</div>
 				<div class="column">
 					<h2>Columna Derecha</h2>
@@ -15,6 +27,6 @@ export const HomePage = () => {
 
 			</div>
 
-		</>
+		</body>
 	)
 }
