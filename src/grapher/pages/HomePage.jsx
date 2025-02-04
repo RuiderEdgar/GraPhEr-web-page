e94562f881e6e07e73d1660bdb57bdf58159f2fb
LuisNavaFisBio <ruisugarciafisbio@ciencias.unam.mx>
2025-02-03 20:13:48 -0600

    changes to homepage

--- src/grapher/pages/HomePage.jsx
+++ src/grapher/pages/HomePage.jsx
@@ -3,7 +3,18 @@ import './HomePage.css'
 export const HomePage = () => {
 	return (
 		<>
-			<div>HomePage</div>
+			<div class="container">
+				<div class="column">
+					<h1>GraPhEr</h1>
+					<h3>Graficadora para Ecuaciones.</h3>
+					<h3>Diferencias Parciales</h3>
+				</div>
+				<div class="column">
+					<h2>Columna Derecha</h2>
+					<p>Contenido de la segunda columna.</p>
+				</div>
+
+			</div>
 
 		</>
 	)
