0bd9fa31a0f0fee04e5baf4a3a9ef418c1e5e58e
RuiderEdgar <edgar.riveraj@gmail.com>
2025-02-04 19:39:24 -0600

    images

--- src/grapher/pages/Acerca.jsx
+++ src/grapher/pages/Acerca.jsx
@@ -1,23 +1,30 @@
-import s from './Acerca.module.css';
+import s from "./Acerca.module.css";
 
 export const AcercaDe = () => {
 	return (
-		<>
-			<main className={s.main}>
-				<section>
-					<header>
-						<h1>Grapher</h1>
-						<h2>Graficadora para Ecuaciones</h2>
-					</header>
-				</section>
+		<main className={s.main_grid}>
+			<section className={s.section_left}>
+				<header className={s.header}>
+					<h1 className={s.title}>GraPhEr</h1>
+					<h2 className={s.subtitle}>
+						Graficadora para Ecuaciones Diferenciales Parciales
+					</h2>
+				</header>
+				<aside>
+					<img className={s.img_grafica2d} src="/assets/images/grafica2d.png" alt="imagen grafica" />
+					<div alt="image"></div>
+					<div alt="texto"></div>
+					<div alt="image"></div>
+				</aside>
+			</section>
 
-				<section>
-					<header>
-						<h1>Grapher</h1>
-						<h2>Graficadora para Ecuaciones</h2>
-					</header>
-				</section>
-			</main>
-		</>
+			<section>
+				<header>
+					<div alt="texto"></div>
+					<div alt="image"></div>
+					<div alt="texto"></div>
+				</header>
+			</section>
+		</main>
 	);
-}
+};
