fc10dfc1183368b4679a66f8915a82883bfce351
LuisNavaFisBio <ruisugarciafisbio@ciencias.unam.mx>
2025-02-04 22:35:07 -0600

    Ultima version homepage

--- src/grapher/pages/HomePage.jsx
+++ src/grapher/pages/HomePage.jsx
@@ -4,29 +4,30 @@ import s from './Homepage.module.css';
 export const HomePage = () => {
 	return (
 		<>	
-			<div className={s.container}>
-				<div className={s.column}>
-					<h1 className={s.name}>GraPhEr</h1>
-					<p className={s.title}>Graficadora para Ecuaciones <br></br> 
-					Diferencias Parciales </p>
+		 	<div className={s.parentContainer}>
+				<div className={s.container}>
+					<div className={s.column}>
+						<h1 className={s.name}>GraPhEr</h1>
+						<p className={s.title}>Graficadora para Ecuaciones <br></br> 
+						Diferencias Parciales </p>
 
-					<p className={s.description}>
-					GraPhEr crea una gráfica interactiva de la solución de una Ecuación Diferencial Parcial a partir de aproximar numéricamente la solución analítica ingresada.
-					<br></br>
-					<br></br>
-					Además, provee diversas herramientas para visualizar y explorar esta gráfica, así como explorar la solución numérica obtenida.	
-					</p>
+						<p className={s.description}>
+						GraPhEr crea una gráfica interactiva de la solución de una Ecuación Diferencial Parcial a partir de aproximar numéricamente la solución analítica ingresada.
+						<br></br>
+						<br></br>
+						Además, provee diversas herramientas para visualizar y explorar esta gráfica, así como explorar la solución numérica obtenida.	
+						</p>
 
-					<input type="s.button" value="Descargar GraPhEr" className={s.download}/>
+						<input type="s.button" value="Descargar GraPhEr" className={s.download}/>
+					</div>
+
+					<div className={s.column}>
+						<canvas className={s.video}> </canvas>
+						<p className={s.learn}>
+						Aprende más sobre los escenarios de uso visitando la sección de <a href="/ejercicios" className={s.learnLink}> Ejercicios</a>.
+						</p>
+					</div>
 				</div>
-
-				<div className={s.column}>
-					<canvas className={s.video}> </canvas>
-					<p className={s.learn}>
-					Aprende más sobre los escenarios de uso visitando la sección de <a href="/ejercicios" className={s.learnLink}> Ejercicios</a>.
-					</p>
-				</div>
-
 			</div>
 		</>
 	)
--- src/ui/components/Footer.jsx
+++ src/ui/components/Footer.jsx
@@ -10,14 +10,13 @@ export const Footer = () => {
                     <div className={s.footerColumnLeft}> 
                         <p>GraPhEr es un software de uso abierto bajo la licencia <a href="https://www.gnu.org/licenses/gpl-3.0.html" target="blank_"> GNU GENERAL PUBLIC LICENSE Versión 3.0 </a></p>
                     </div>
-                    
-                    <div className={s.footerInfo}>
-                        <div className={s.footerColumnRight}>
-                            <p> © Luis Enrique Nava Garcia - 2025 </p> 
-                        </div>
-                        <div className={s.footerColumnRight}>
-                            <a href="https://github.com/LuisNavaFisBio/GraPhEr" target="blank_"> <img src="../../../assets/icons/github-mark.svg" className={s.footerImage} /> </a>
-                        </div>
+
+                    <div className={s.footerColumnRight}>
+                        <p> © Luis Enrique Nava Garcia - 2025 </p> 
+                    </div>
+
+                    <div className={s.footerContainerImage}>
+                        <a href="https://github.com/LuisNavaFisBio/GraPhEr" target="blank_" className={s.footerImageLink}> <img src="../../../assets/icons/github-mark.svg" className={s.footerImage} /> </a>
                     </div>
                 </div>
             </footer>
