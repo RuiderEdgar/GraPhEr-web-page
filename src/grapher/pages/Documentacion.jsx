ba82710c48a9452cb152844dc5896311d2d39851
RuiderEdgar <edgar.riveraj@gmail.com>
2025-08-02 19:07:37 -0600

    last changes

--- src/grapher/pages/Documentacion.jsx
+++ src/grapher/pages/Documentacion.jsx
@@ -1,12 +1,25 @@
 import { Temario, ContentDocumentation } from './components';
 import { FixKatex } from '../helpers';
 import s from './Documentacion.module.css';
-
+import { useState } from 'react';
+//  ToDo: arreglar el sticky del contentdocumentacion, posible idea, meter el contentDocumentation en un contenedor
 export const Documentacion = () => {
+	const [menuVisible, setMenuVisible] = useState(false);
 	return (
 		<main className={s.main_container}>
+			{/* Botón para abrir/cerrar menú */}
+			<button
+				className={`${s.menu_button} ${menuVisible ? s.show_button : ""}`}
+				onClick={() => setMenuVisible(!menuVisible)}
+			>
+				{menuVisible ? "Cerrar" : "Menú"}
+			</button>
+
 			{/* ----------menu seccion-------------- */}
-			<aside className={s.temario} alt="temario">
+			<aside
+				className={`${s.temario} ${menuVisible ? s.show : ""}`}
+				alt="temario"
+			>
 				<div className={s.title_container}>
 					<h1 className={s.title}>GraPhEr 1.0</h1>
 				</div>
@@ -16,8 +29,7 @@ export const Documentacion = () => {
 			{/* ----------documentacion seccion---------- */}
 			<article className={s.contenido}>
 				<ContentDocumentation />
-				<FixKatex/>
-				{/* ToDo: arreglar el sticky del contentdocumentacion, posible idea, meter el contentDocumentation en un contenedor */}
+				<FixKatex />
 			</article>
 		</main>
 	);
