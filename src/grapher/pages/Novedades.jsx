3aaa06b9fa2d23ee96db2e301ec16149b55dae4a
Luis Enrique Nava Garcia <ruisugarciafisbio@ciencias.unam.mx>
2025-08-02 21:33:16 -0600

    Changes to Novedades page

--- src/grapher/pages/Novedades.jsx
+++ src/grapher/pages/Novedades.jsx
@@ -9,6 +9,9 @@ export const Novedades = () => {
 				<h1 className={s.title}>Última Actualización</h1>
 				<section className={s.grid_container}>
 					<article className={s.article1}>
+						
+					</article>
+					<article className={s.article1}>
 						<p>
 							Hemos añadido los botones Importar y Exportar en la ventana
 							principal de la aplicación.
@@ -18,11 +21,17 @@ export const Novedades = () => {
 							contenidas en un archivo de texto.
 						</p>
 					</article>
+					<article className={s.article1}>
+						
+					</article>
 					<aside className={s.img_container}>
-						<img src="" alt="imagen de app" />
+						<img className={s.image1} src="../assets/images/Novedades1_V1_0.png" alt="imagen de app" />
 					</aside>
+					<article className={s.article1}>
+						
+					</article>
 					<aside className={s.video_container}>
-						<video src="" alt="video de la actualización"></video>
+						<video className={s.video1} src="../assets/images/Novedades.mp4" alt="video de la actualización" controls  muted></video>
 					</aside>
 					<article className={s.article2}>
 						<p>
