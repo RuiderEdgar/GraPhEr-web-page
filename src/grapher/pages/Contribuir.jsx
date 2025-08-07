7f7dacb56b334ac3160052a7440fcf076dc54544
Luis Enrique Nava Garcia <ruisugarciafisbio@ciencias.unam.mx>
2025-08-06 23:08:00 -0600

    Contribue responsive design first changes

--- src/grapher/pages/Contribuir.jsx
+++ src/grapher/pages/Contribuir.jsx
@@ -6,7 +6,7 @@ export const Contribuir = () => {
 		<>
 			<div className={s.parentContainer}>
 				<div className={s.container}>
-					<div className={s.row1}>
+					<div className={s.row}>
 						<div className={s.leftColumn}>
 							<p className={s.title}> ¿Quieres modificar el código? </p>
 							<p className={s.text}> Abre un fork de nuestro repositorio en <a href="https://github.com/LuisNavaFisBio/GraPhEr" target='_blank' className={s.learnLink}>Github</a> y comparte tus sugerencias de mejoras en el código dando una explicación detallada. <br></br>
@@ -17,8 +17,8 @@ export const Contribuir = () => {
 						</div>
 					</div>
 
-					<div className={s.row2}>
-						<div className={s.leftColumn2}>
+					<div className={s.row}>
+						<div className={s.leftColumn}>
 							<p className={s.title2}> ¿Quieres trasladar el código a otros lenguajes? </p>
 							<p className={s.text}>Descarga el código desde el repositorio en <a href="https://github.com/LuisNavaFisBio/GraPhEr" target='_blank' className={s.learnLink}>Github</a> y crea su análogo en el lenguaje de programación de tu interés.<br></br>
 							Envía tu versión a nuestro equipo para comprobar que funciona correctamente. Si todo está en orden, publicaremos tu versión en la sección <a href="/descargar" className={s.learnLink}>Descargar</a> dándote todo el crédito, adicionalmente, crearemos un apartado en nuestro repositorio para que otros usuarios puedan modificar tu versión.</p>
@@ -28,8 +28,8 @@ export const Contribuir = () => {
 						</div>
 					</div>
 
-					<div className={s.row3}>
-						<div className={s.leftColumn3}>
+					<div className={s.row}>
+						<div className={s.leftColumn}>
 							<p className={s.title3}> ¿Quieres distribuir la aplicación a dispositivos móviles u otros sistemas operativos? </p>
 							<p className={s.text}> Descarga el código en Python desde nuestro repositorio en <a href="https://github.com/LuisNavaFisBio/GraPhEr" target='_blank' className={s.learnLink}>Github</a> y comparte el proceso de creación del archivo instalador para el sistema operativo. Todo el proceso debe estar explicado a detalle, con esto nos aseguramos que el archivo es seguro para todos los usuarios. Una vez verificado, nuestro equipo seguirá las instrucciones y podrá crear el archivo instalador. <br></br>
 							En el caso de dispositivos móviles, revisa el estado actual del proyecto en el repositorio de Github <br></br> <a href="https://github.com/LuisNavaFisBio/GraPhEr_mobileApp" target='_blank' className={s.learnLink} > GraPhEr para dispositivos móviles</a> y comparte tus aportaciones a través de un fork, el equipo se encargará de revisar tus sugerencias.</p>
